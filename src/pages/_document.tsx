d5e4767123553860cd2a8cad765d01ba4d0a74f9
donovanbrun <donovanbrun@gmail.com>
2023-05-25 09:08:33 +0200

    title and favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html>
-            <Head />
+            <Head>
+                <title>Organizr</title>
+                <link rel="shortcut icon" href="/icon.png" type="image/x-icon" />
+            </Head>
             <body>
                 <Main />
                 <NextScript />
